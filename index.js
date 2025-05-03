fb2f4ab13a2cabef9e6879dde3e6eda4aa6437af
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-05-03 11:05:10 +0600

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const path = require("path");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv");const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 dotenv.config({ path: path.join(process.cwd(), ".env") });
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.post("/", async (req, res) => {
-  const { to, subject, html } = req.body;
+  const { to, subject, html, nodemailer_host_email, nodemailer_host_pass } =
+    req.body;
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", // Fixed: Removed the extra dot here
+    host: "smtp.gmail.com.", // Fixed: Removed the extra dot here
     port: 587,
-    secure:false, // process.env.NODE_ENV === "production"
+    secure: false, // process.env.NODE_ENV === "production"
     auth: {
-      user: process.env.NODEMAILER_HOST_EMAIL,
-      pass: process.env.NODEMAILER_HOST_PASS,
+      user: nodemailer_host_email,
+      pass: nodemailer_host_pass,
     },
   });
 
   try {
     await transporter.sendMail({
-      from: "nurmdopu428@gmail.com",
+      from: nodemailer_host_email,
       to,
       subject,
       text: "",
